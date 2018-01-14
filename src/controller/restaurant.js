8a74e63be3d5e5abc094eeba8077cd5f76ad8f77
maylcf <mayaralcabral@gmail.com>
2018-01-14 00:24:04 -0500

    Search, Update and Remove Restaurants
    - Search restaurants
    - Update restaurant
    - Delete Restaurant

--- src/controller/restaurant.js
+++ src/controller/restaurant.js
@@ -18,5 +18,51 @@ export default ({ config, db }) => {
     });
   });
 
+  // '/v1/restaurant' - Read
+  api.get('/', (req, res) => {
+    Restaurant.find({}, (err, restaurants) => {
+      if (err) {
+        res.send(err);
+      }
+      res.json(restaurants);
+    });
+  });
+
+  // '/v1/restaurant/:id' - Read 1
+  api.get('/:id', (req, res) => {
+    Restaurant.findById(req.params.id, (err, restaurant) => {
+      if (err) {
+        res.send(err);
+      }
+      res.json(restaurant);
+    });
+  });
+
+  // '/v1/restaurant/:id' - Update
+  api.put('/:id', (req, res) => {
+    Restaurant.findById(req.params.id, (err, restaurant) => {
+      if (err) {
+        res.send(err);
+      }
+      restaurant.name = req.body.name;
+      restaurant.save(err => {
+        if (err) {
+          res.send(err);
+        }
+        res.json({ message: "Restaurant info updated"});
+      });
+    });
+  });
+
+  // '/v1/restaurant/:id' - Delete
+  api.delete('/:id', (req, res) => {
+    Restaurant.remove({ _id: req.params.id }, (err, restaurant) => {
+      if (err) {
+        res.send(err);
+      }
+      res.json({ message: "Restaurant Successfully Removed!"});
+    });
+  });
+
   return api;
 }
